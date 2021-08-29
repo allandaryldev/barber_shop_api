5a74e8902c9cb348a6a0f7f0fbc8d8c938ba8805
allandaryldev <87156467+allandaryldev@users.noreply.github.com>
2021-08-29 23:29:52 +0800

    trans req and res patch

--- app/Http/Resources/TransactionsResource.php
+++ app/Http/Resources/TransactionsResource.php
@@ -4,7 +4,7 @@ namespace App\Http\Resources;
 
 use App\Models\Transaction_items;
 use Illuminate\Http\Resources\Json\JsonResource;
-
+use Carbon\Carbon;
 class TransactionsResource extends JsonResource
 {
     /**
@@ -16,9 +16,13 @@ class TransactionsResource extends JsonResource
     public function toArray($request)
     {
         return [
+            "id"=>$this->id,
             "user"=>new UsersResource($this->user),
             "customer"=>new CustomersResource($this->customer),
-            "items"=>Transaction_items::collection($this->items),
+            "items"=>Transaction_itemsResource::collection($this->items),
+            // "total"=>number_format($this->total,2),
+            "total"=>$this->total,
+            "created_at"=>Carbon::parse($this->created_at)->format('M d, Y '),
         ];
     }
 }
