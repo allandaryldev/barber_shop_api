bbb7c06931d2d243ab81c673c2424060486c5dad
allandaryldev <87156467+allandaryldev@users.noreply.github.com>
2021-09-01 09:23:00 +0800

    web update

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('{path}', function () {
+    return file_get_contents(public_path('index.html'));
+})->where('path', '(.*)');
